2c5daa1b7bdfce2d84b6718739803c3fc394e08b
mutsuki <mutsuki@momo86.net>
2022-08-11 13:51:57 +0900

    Add a test for `store`

--- test/main.cpp
+++ test/main.cpp
@@ -10,10 +10,18 @@ int test(
 
 	auto dataset_uptr = std::unique_ptr<data_t[]>(new data_t[num_data * data_dim]);
 
+	const auto format = mtk::anns_dataset::detect_file_format<data_t>(dataset_path);
+
 	if (mtk::anns_dataset::load(dataset_uptr.get(), dataset_path, true)) {
-		std::printf("Failed\n");
+		std::printf("Failed @l.%d\n", __LINE__);
 		return 1;
 	}
+
+	if (mtk::anns_dataset::store(dataset_path + ".1", 1, data_dim, dataset_uptr.get(), format)) {
+		std::printf("Failed @l.%d\n", __LINE__);
+		return 1;
+	}
+
 	std::printf("Succeeded\n");
 
 	return 0;
